0f808d0e23247c2f3f82b84805d1163e61afcef3
souliyaLITD <souliya.litd@gmail.com>
2025-02-21 10:52:36 +0700

    🐞 fix(refrest page view details of products):

--- app/containers/admin/products/ProductList.tsx
+++ app/containers/admin/products/ProductList.tsx
@@ -18,12 +18,12 @@ const ProductCard = React.memo(({ product }: { product: Products }) => {
   };
 
   return (
-    <div className='relative'>
+    <>
       <Badge
         size='lg'
         color='primary'
         className='px-2 py-1 text-xs sm:text-sm md:text-base font-bold rounded-full absolute top-3 right-3 z-10'
-        content={product.quantity}
+        content={product?.quantity}
       >
         <Link
           to={params}
@@ -41,29 +41,29 @@ const ProductCard = React.memo(({ product }: { product: Products }) => {
             {/* Product Image */}
             <CardBody className='p-0'>
               <Image
-                alt={product.name}
+                alt={product?.name}
                 src={
                   product?.imageurl?.[0]
                     ? product?.imageurl?.[0]
                     : 'https://th.bing.com/th/id/OIP.mhEjokf4cHBCeCsOqohUdwHaHa?rs=1&pid=ImgDetMain'
                 }
-                className='w-[175px] sm:w-[160px] md:w-[180px] object-cover h-[170px] sm:h-[160px] md:h-[180px] rounded-t-lg'
+                className='w-[140px] sm:w-[160px] md:w-[180px]object-cover h-[110px] sm:h-[160px] md:h-[180px] rounded-t-lg'
               />
             </CardBody>
 
             {/* Product Details */}
             <CardFooter className='flex flex-col items-center px-1 py-1 space-y-1'>
               <b className='text-center w-full max-w-[100px] truncate'>
-                {product.name}
+                {product?.name}
               </b>
               <p className='text-default-500 text-center w-full max-w-[100px] truncate'>
-                ${product.price}
+                ${product?.price}
               </p>
             </CardFooter>
           </Card>
         </Link>
       </Badge>
-    </div>
+    </>
   );
 });
 
@@ -74,7 +74,7 @@ export default function ProductList({ products }: { products: Products[] }) {
         <p className='text-center text-gray-500 text-lg'>No products found</p>
       ) : (
         /* Desktop/Tablet/Mobile Grid View */
-        <div className='gap-2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
+        <div className='gap-2 grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
           {products.map((product, index) => (
             <ProductCard key={index} product={product} />
           ))}
